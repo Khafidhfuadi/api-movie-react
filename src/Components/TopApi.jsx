eded0b3218df1f55b7e0d59af90094860bfdf12f
khafidhfuadi <muhammadkhafidfuadi@gmail.com>
2022-07-21 16:04:42 +0700

    changes api

--- src/Components/TopApi.jsx
+++ src/Components/TopApi.jsx
@@ -3,21 +3,19 @@ import React, { useState, useEffect } from "react";
 
 const baseURL = `https://api.themoviedb.org/3/movie/popular?api_key=31a6b18e67bfaf12776d9a9e364d02a5`;
 
-const TopSection = () => {
+const TopApi = () => {
   const [movies, setMovies] = useState([]);
 
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
       setMovies(response.data.results);
-      console.log(movies[0]);
+      console.log(response.data.results);
     });
   }, []);
 
-  if (!movies) return null;
-
   return (
     <div className="container-fluid movie-app">
-      {/* <div className="row">{console.log(movies.results[0].original_title)}</div> */}
+      <div className="row">{movies[0]?.original_title}</div>
     </div>
   );
 };
