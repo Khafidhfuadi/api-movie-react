54ba324cf7d678448ad0032e98ff3faedfa587e6
kemasghani <kemasghani123@gmail.com>
2022-07-21 15:27:13 +0700

    perubahan ghani

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,5 @@ import { useState } from "react";
 
 export default function NavBar() {
   const [navbar, setNavbar] = useState(false);
-
   return <nav className="w-full #1a1c2d shadow">navbar pull</nav>;
 }
---
+++ src/Components/TopMovie.jsx
@@ -0,0 +1,7 @@
+import styles from "../styling/TopMovie.css";
+function TopMovie(){
+    <div className={styles.containerTopMovie}>
+        <div className={styles.cardTopMovie}></div>
+    </div>
+}
+export default TopMovie;
